8551bb5b19dd50b2a7dc73640e28d02b6b458343
Robin Millette <robin@millette.info>
2024-03-05 17:01:36 -0500

    Convert to fully static version.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	output: 'server',
-	adapter: nodejs({ mode: 'standalone' }),
+	site: 'https://millette.github.io',
+	base: '/nistro',
 	vite: {
 		define: {
 			'process.env.TMDB_API_KEY': JSON.stringify(process.env.TMDB_API_KEY),
